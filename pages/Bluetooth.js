6ae51cdf38d6c2352cd674f9fbf855ac469a2647
LubabaSheikh <lubaba.sheikh01@gmail.com>
2024-03-25 12:53:27 -0600

    Bluetooth pressure changes

--- pages/Bluetooth.js
+++ pages/Bluetooth.js
@@ -58,7 +58,12 @@ export default function Bluetooth({navigation}) {
   //=== Bluetooth Setup ===//
   const [deviceID, setDeviceID] = useState(null);
   const [sweatValue, setSweatValue] = useState(0);
-  const [pressureValue, setPressureValue] = useState(0);
+  const [pressureValue1, setPressureValue1] = useState(0);
+  const [pressureValue2, setPressureValue2] = useState(0);
+  const [pressureValue3, setPressureValue3] = useState(0);
+  const [pressureValue4, setPressureValue4] = useState(0);
+  const [pressureValue5, setPressureValue5] = useState(0);
+  const [pressureValue6, setPressureValue6] = useState(0);
   const [sweatDataChar, setSweatDataChar] = useState(null);
   const [pressureDataChar, setPressureDataChar] = useState(null);
   const [connectionStatus, setConnectionStatus] = useState("Searching Device ...");
@@ -98,21 +103,6 @@ useEffect(() => {
     });
   };
 
-  // Dont need this for refresh
-  // Function to refresh the cache of the selected device
-  // const refreshDeviceCache = async () => {
-  //   if (deviceRef.current) {
-  //     try {
-  //       await deviceRef.current.refreshCache();
-  //       console.log("Device cache refreshed successfully.");
-  //     } catch (error) {
-  //       console.error("Error refreshing device cache:", error);
-  //     }
-  //   } else {
-  //     console.error("No connected device to refresh cache.");
-  //   }
-  // };
-
   useEffect(() => {
     searchAndConnectToDevice();
   }, []);
@@ -151,15 +141,25 @@ useEffect(() => {
             console.log("Received data:", rawData);
             
             const splitResult = rawData.toString().split('|');
-            const rawSweatData = splitResult[0]; // '40.1 C'
-            const rawPressureData = splitResult[1];    // '75kPa'
+            const rawPressureData1 = splitResult[0];    // '75kPa'
+            const rawPressureData2 = splitResult[1];    // '75kPa'
+            const rawPressureData3 = splitResult[2];    // '75kPa'
+            const rawPressureData4 = splitResult[3];    // '75kPa'
+            const rawPressureData5 = splitResult[4];    // '75kPa'
+            const rawPressureData6 = splitResult[5];    // '75kPa'
+            const rawSweatData = splitResult[6]; // '40.1 C'
             setSweatValue(rawSweatData); // Set the sweat value here
-            setPressureValue(rawPressureData); // Set the sweat value here
+            setPressureValue1(rawPressureData1); // Set the pressure value here
+            setPressureValue2(rawPressureData2); // Set the pressure value here
+            setPressureValue3(rawPressureData3); // Set the pressure value here
+            setPressureValue4(rawPressureData4); // Set the pressure value here
+            setPressureValue5(rawPressureData5); // Set the pressure value here
+            setPressureValue6(rawPressureData6); // Set the pressure value here
 
             if (rawSweatData != null)
             {
                 try {
-                    const glucoseResponse = await axios.post(`https://7a5f-136-159-213-241.ngrok-free.app/add_glucose_value/${username}`, {
+                    const glucoseResponse = await axios.post(`https://2232-2604-3d09-3472-7800-1da4-da3b-2ce9-4dea.ngrok-free.app/add_glucose_value/${username}`, {
                     glucose: rawSweatData,
                 });
                 console.log('USERNAME: ' + username)
@@ -172,11 +172,16 @@ useEffect(() => {
                     console.error('Error Blutooth Glucose Post Request:', error);
                 }                
             }
-            if (rawPressureData != null)
+            if (rawPressureData1 != null)
             {
                 try {
-                    const pressureResponse = await axios.post(`https://7a5f-136-159-213-241.ngrok-free.app/add_pressure_value/${username}`, {
-                    pressure: rawPressureData,
+                    const pressureResponse = await axios.post(`https://2232-2604-3d09-3472-7800-1da4-da3b-2ce9-4dea.ngrok-free.app/add_pressure_value/${username}`, {
+                    p1: rawPressureData1,
+                    p2: rawPressureData2,
+                    p3: rawPressureData3,
+                    p4: rawPressureData4,
+                    p5: rawPressureData5,
+                    p6: rawPressureData6,
                 });
                 if (pressureResponse.data.success) {
                     console.log("Updated database successfully");   
@@ -232,7 +237,12 @@ useEffect(() => {
         <Text style={styles.text}>I-SOLE Bluetooth</Text>
         <Text style={styles.text}>Connection Status: {connectionStatus}</Text>
         <Text style={styles.text}>Sweat Value: {sweatValue}</Text>
-        <Text style={styles.text}>Pressure Value: {pressureValue}</Text>
+        <Text style={styles.text}>Pressure Value 1: {pressureValue1}</Text>
+        <Text style={styles.text}>Pressure Value 2: {pressureValue2}</Text>
+        <Text style={styles.text}>Pressure Value 3: {pressureValue3}</Text>
+        <Text style={styles.text}>Pressure Value 4: {pressureValue4}</Text>
+        <Text style={styles.text}>Pressure Value 5: {pressureValue5}</Text>
+        <Text style={styles.text}>Pressure Value 6: {pressureValue6}</Text>
         <StatusBar style="auto" />
         
       
