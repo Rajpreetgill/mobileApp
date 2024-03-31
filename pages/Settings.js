9b95423d6f5db68b57720c2bfdbaf6d259ce7aae
LubabaSheikh <lubaba.sheikh01@gmail.com>
2024-03-31 16:42:02 -0600

    Finished settings page

--- pages/Settings.js
+++ pages/Settings.js
@@ -1,23 +1,155 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, Switch } from 'react-native';
 import Button from '../components/Button';
 import Icon from 'react-native-vector-icons/AntDesign';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import { useFocusEffect } from '@react-navigation/native';
+
 
 export default function Settings({navigation}) {
   const [notifications, setNotifications] = useState(false);
   const [allowActivity, setAllowActivity] = useState(false);
   const [allowMeals, setAllowMeals] = useState(false);
-  const [allowDoctorsFeedback, setAllowDoctorsFeedback] = useState(false);
+  const [allowFeedback, setAllowFeedback] = useState(false);
+  const [username, setUsername] = useState('');
 
-  const toggleSwitch = (toggleFunction) => {
-    toggleFunction((prevState) => !prevState);
+ 
+
+  useFocusEffect(
+    React.useCallback(() => {
+      const fetchData = async () => {
+        try {
+          const username = await AsyncStorage.getItem('curr_username');
+          setUsername(username);
+          getSettingsData(username); // You can call getProfileData here
+        } catch (e) {
+          // Handle errors here
+          console.error("Error retrieving data", e);
+        }
+      };
+      fetchData();
+      console.log("Fetch notification:", notifications);
+    }, [getSettingsData]) 
+  );
+
+  const getSettingsData = async (username) => {
+    try {
+      const response = await axios.get(`https://i-sole-backend.com/get_profile_data/${username}`);
+      if (response.data.success) {
+          // Do something with the retrieved profile data
+          const userData = response.data.data;
+          if(userData.notifications != null)
+          {
+            setNotifications(userData.notifications);
+          }
+          if(userData.view_activity != null)
+          {
+            setAllowActivity(userData.view_activity);
+          }
+          if(userData.view_meals != null)
+          {
+            setAllowMeals(userData.view_meals);
+          }
+          if(userData.view_feedback != null)
+          {
+            setAllowFeedback(userData.view_feedback);
+          }
+
+      } else {
+          // Update failed
+          console.error('Profile data retrieval failed:', response.data.message);
+      }
+    } catch (error) {
+        //console.error('Error retrieving profile data:', error);
+    }
+  }
+
+  const toggleNotificationSwitch = async () => {
+    try {
+      const response = await axios.post('https://i-sole-backend.com/update_notifications', {
+        username: username,
+        value: !notifications
+      });
+      if(response.data.success)
+      {
+        console.log('Successfully updated notifications!');
+      }
+      else
+      {
+        console.log("Error updating notifications: ", response.data.message);
+      }
+    } catch (error) {
+      console.error('Error setting notifications:', error);
+    }
+    setNotifications((prevState) => !prevState);
+  };
+
+  const toggleViewActivitySwitch = async () => {
+    try {
+      const response = await axios.post('https://i-sole-backend.com/update_view_activity', {
+        username: username,
+        value: !allowActivity
+      });
+      if(response.data.success)
+      {
+        console.log('Successfully updated allow activity!');
+      }
+      else
+      {
+        console.log("Error updating allow activity: ", response.data.message);
+      }
+    } catch (error) {
+      console.error('Error setting allow activity:', error);
+    }
+    setAllowActivity((prevState) => !prevState);
+  };
+
+  const toggleViewMealsSwitch = async () => {
+    try {
+      const response = await axios.post('https://i-sole-backend.com/update_view_meals', {
+        username: username,
+        value: !allowMeals
+      });
+      if(response.data.success)
+      {
+        console.log('Successfully updated allow meals!');
+      }
+      else
+      {
+        console.log("Error updating allow meals: ", response.data.message);
+      }
+    } catch (error) {
+      console.error('Error setting allow meals:', error);
+    }
+    setAllowMeals((prevState) => !prevState);
+  };
+
+  const toggleViewFeedbackSwitch = async () => {
+    try {
+      const response = await axios.post('https://i-sole-backend.com/update_view_feedback', {
+        username: username,
+        value: !allowFeedback
+      });
+      if(response.data.success)
+      {
+        console.log('Successfully updated allow feedback!');
+      }
+      else
+      {
+        console.log("Error updating allow feedback: ", response.data.message);
+      }
+    } catch (error) {
+      console.error('Error setting allow feedback:', error);
+    }
+    setAllowFeedback((prevState) => !prevState);
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.container}>
-        <Text style={styles.text}>Settings Page</Text>
+        <Text style={styles.text}>Settings</Text>
           {/* Settings Gear Icon */}
           <Icon name="setting" style={styles.goldenIcon} />
       </View>
@@ -31,7 +163,7 @@ export default function Settings({navigation}) {
           <Switch
             trackColor={{ false: '#DEB992', true: '#1BA098' }}
             thumbColor={notifications ? '#DEB992' : '#1BA098'}
-            onValueChange={() => toggleSwitch(setNotifications)}
+            onValueChange={toggleNotificationSwitch}
             value={notifications}
           />
         </View>
@@ -41,7 +173,7 @@ export default function Settings({navigation}) {
           <Switch
             trackColor={{ false: '#DEB992', true: '#1BA098' }}
             thumbColor={allowActivity ? '#DEB992' : '#1BA098'}
-            onValueChange={() => toggleSwitch(setAllowActivity)}
+            onValueChange={toggleViewActivitySwitch}
             value={allowActivity}
           />
         </View>
@@ -51,7 +183,7 @@ export default function Settings({navigation}) {
           <Switch
             trackColor={{ false: '#DEB992', true: '#1BA098' }}
             thumbColor={allowMeals ? '#DEB992' : '#1BA098'}
-            onValueChange={() => toggleSwitch(setAllowMeals)}
+            onValueChange={toggleViewMealsSwitch}
             value={allowMeals}
           />
         </View>
@@ -60,9 +192,9 @@ export default function Settings({navigation}) {
           <Text style={styles.toggleText}>Allow Family Members to See Doctors Feedback</Text>
           <Switch
             trackColor={{ false: '#DEB992', true: '#1BA098' }}
-            thumbColor={allowDoctorsFeedback ? '#DEB992' : '#1BA098'}
-            onValueChange={() => toggleSwitch(setAllowDoctorsFeedback)}
-            value={allowDoctorsFeedback}
+            thumbColor={allowFeedback ? '#DEB992' : '#1BA098'}
+            onValueChange={toggleViewFeedbackSwitch}
+            value={allowFeedback}
           />
         </View>
 
@@ -111,8 +243,9 @@ const styles = StyleSheet.create({
         paddingBottom: 10, // Add padding to the bottom
     },
     text: {
-        color: '#DEB992',
-        fontSize: 30,
+      color: '#DEB992',
+      fontSize: 35,
+      fontWeight: 'bold',
     },
     userIcon: {
         marginLeft: 10,
@@ -160,7 +293,7 @@ const styles = StyleSheet.create({
       color: '#DEB992',
       fontSize: 18,
       marginRight: 70, // Adjust this value to increase or decrease the distance
-      marginLeft: -15 // Adjust this value to increase or decrease the distance
+      marginLeft: -15, // Adjust this value to increase or decrease the distance
     },
     iconContainerBottom: {
       flexDirection: 'row',
